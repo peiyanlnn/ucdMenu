3bcd99f2b49998a27fef51fb3a2e41ec72425e57
peiyanlnn <peiyanlnn@gmail.com>
2024-02-15 21:24:33 +0800

    simple project for menu query of UCD

---
+++ src/main/java/com/example/ucdmenu/UcdMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.ucdmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcdMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcdMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ucdmenu/UcdMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ucdmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcdMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
